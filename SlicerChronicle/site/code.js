f2b3c6dadedc179b0d907897551b2a744d18bee9
Steve Pieper <pieper@bwh.harvard.edu>
2015-04-06 10:39:14 -0400

    Switch from jquery.couch to pouchdb

--- SlicerChronicle/site/code.js
+++ SlicerChronicle/site/code.js
@@ -3,7 +3,10 @@ var table;
 $(function(){ // on dom ready
 
 
-  $.couch.urlPrefix = "http://common.bwh.harvard.edu:5984";
+  var chURLPrefix = "http://common.bwh.harvard.edu:5984";
+  var chDatabase = "chronicle";
+
+  var chronicle = new PouchDB(chURLPrefix+'/'+chDatabase);
 
   // http://www.datatables.net/examples/api/multi_filter.html
   var addColumnSearch = function (tableID) {
@@ -26,23 +29,22 @@ $(function(){ // on dom ready
 
   // Series
   studyDataTable = $('#studyDataTable').DataTable();
-  $.couch.db("chronicle").view("instances/context", {
+  chronicle.query("instances/context", {
     reduce : true,
     group_level : 2,
     //limit: 5,
-    success: function(data) {
-      // add table entries for each hit
-      $.each(data.rows, function(index,row) {
-        var institution = row.key[0][0];
-        var patientUID = String(row.key[0]);
-        var patientID = row.key[0][1];
-        var studyDescription = row.key[1][0];
-        var studyUID = row.key[1][1];
-        var seriesCount = row.value;
-        studyDataTable.row.add(['', patientID, studyDescription]).draw();
-      });
-      addColumnSearch('#studyDataTable');
-    }
+  }).then(function(data) {
+    // add table entries for each hit
+    $.each(data.rows, function(index,row) {
+      var institution = row.key[0][0];
+      var patientUID = String(row.key[0]);
+      var patientID = row.key[0][1];
+      var studyDescription = row.key[1][0];
+      var studyUID = row.key[1][1];
+      var seriesCount = row.value;
+      studyDataTable.row.add(['', patientID, studyDescription]).draw();
+    });
+    addColumnSearch('#studyDataTable');
   });
 
 
@@ -51,26 +53,25 @@ $(function(){ // on dom ready
   instanceDataTable = $('#instanceDataTable').DataTable();
 
   var i;
-  for (i = 0; i < 1100; i++) {
+  for (i = 0; i < 11000; i++) {
     instanceDataTable.row.add(['test', String(i)]);
   }
   instanceDataTable.draw();
   addColumnSearch('#instanceDataTable');
   return;
 
-  $.couch.db("chronicle").view("instances/context", {
+  chronicle.query("instances/context", {
     reduce : true,
     group_level : 4,
     limit : 1000,
-    success: function(data) {
-      // add table entries for each hit
-      $.each(data.rows, function(index,row) {
-        var modality = row.key[2][0];
-        var instanceUID = row.key[3];
-        //instanceDataTable.row.add([modality, instanceUID]);
-      });
-      instanceDataTable.draw();
-    }
+  }).then(function(data) {
+    // add table entries for each hit
+    $.each(data.rows, function(index,row) {
+      var modality = row.key[2][0];
+      var instanceUID = row.key[3];
+      //instanceDataTable.row.add([modality, instanceUID]);
+    });
+    instanceDataTable.draw();
   });
 
   $('#studyDataTable tbody').on('click', 'tr', function() {
@@ -78,55 +79,5 @@ $(function(){ // on dom ready
   });
 
   return;
-
-function fetchStudyEdge(id) {
-  $.couch.db("chronicle").openDoc(id, {
-    success : function(data) {
-      var noDotsID = id.replace(/\./g, '_');
-      studyUID = data.dataset['0020000D'].Value;
-      var noDotsStudyID = studyUID.replace(/\./g, '_');
-      cy.add({
-        group : "edges",
-        data : { id : noDotsID+'_to_'+noDotsStudyID, source : noDotsID, target : noDotsStudyID},
-      });
-
-      console.log(data.dataset['0020000D']);
-    },
-    error : function(status) {
-      console.log(status);
-    }
-  });
-};
-
-var value = "Slicer Study Render";
-$.couch.db("chronicle").view("tags/byTagAndValue", {
-  startkey: ["0008103E", value],
-  endkey: ["0008103E", value+"\u9999"],
-  reduce : false,
-  stale : 'update_after',
-  //limit : 30,
-  success: function(data) {
-    $.each(data.rows, function(index,row) {
-      var id = row.id;
-      var imageURL = 'http://common.bwh.harvard.edu:5984/chronicle/' + id + '/image.jpg';
-      var group = "nodes";
-      var noDotsID = id.replace(/\./g, '_');
-      var cyID = '#' + noDotsID;
-      cy.add({
-          group: group,
-          data: { id: noDotsID, weight: 75 },
-          position: { x: 200, y: 200 }
-      });
-      cy.$(cyID).css('shape', 'rectangle');
-      /*
-      cy.$(cyID).css('background-image', imageURL);
-      cy.$(cyID).css('width', '1200px');
-      cy.$(cyID).css('height', '1200px');
-      */
-      fetchStudyEdge(id);
-    });
-    cy.layout({name: "grid"});
-  },
-});
 
 }); // on dom ready
